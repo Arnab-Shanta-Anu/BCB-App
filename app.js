a91c45f07cad6debbbfdaa7c3e54609b5fcac4bb
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2022-09-15 21:28:27 +0600

    added ejs views

--- app.js
+++ app.js
@@ -2,25 +2,49 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 app.listen(5000);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.redirect("/login");
+  res.render("index");
 });
 
-app.get("/all", (req, res) => {
-  res.sendFile(__dirname + "/views/all-companies.html");
+//view all companies
+app.get("/company/all", (req, res) => {
+  res.render("all-companies");
 });
 
-app.get("/all/:name", (req, res) => {
-  res.sendFile(__dirname + "/views/company.html");
+//view single company
+app.get("/company/:name", (req, res) => {
+  res.render("company");
+});
+
+//add a company
+app.post("/company/:name", (req, res) => {
+  res.send("company add req");
+});
+
+//delete a company
+app.delete("/company/:name", (req, res) => {
+  res.send("company delete req");
+});
+
+app.post("/company/modify/:name", (req, res) => {
+  res.send("company modify req");
+});
+
+app.get("/company/summery/:name", (req, res) => {
+  res.remder("company");
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/views/login.html");
+  res.render("login");
 });
 
 //404
 app.use((req, res) => {
-  res.status(404).sendFile(__dirname + "/views/404.html");
+  res.status(404).render("404");
 });
