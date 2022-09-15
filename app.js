ddf509773108c13f7e80cc40ed819d02c49a34b8
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2022-09-16 01:26:45 +0600

    added schema

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoute");
 const companyRoute = require("./routes/companyRoute");
 const summeryRoute = require("./routes/summeryRoute");
+
+const dbURI = "mongodb://localhost:27017/BCB-App";
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("connected to db");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.use(express.json());
@@ -9,8 +21,6 @@ app.use(express.static("public"));
 
 //register view engine
 app.set("view engine", "ejs");
-
-app.listen(5000);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
