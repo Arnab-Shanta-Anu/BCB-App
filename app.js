f1005093e57802d573424e0b35d4ddf5b5867079
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2022-09-15 20:59:46 +0600

    added routing

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ app.listen(5000);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 app.get("/all", (req, res) => {
   res.sendFile(__dirname + "/views/all-companies.html");
 });
-app.get("/:name", (req, res) => {
+
+app.get("/all/:name", (req, res) => {
   res.sendFile(__dirname + "/views/company.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+//404
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/views/404.html");
 });
