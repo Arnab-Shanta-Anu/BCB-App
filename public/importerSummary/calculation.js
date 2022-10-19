7d8e765f8459b19b632863da9191a5f0fd8907a2
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2022-10-19 19:31:21 +0600

    refractoring

---
+++ public/importerSummary/calculation.js
@@ -0,0 +1,108 @@
+function notZero(number) {
+  number = +number;
+  if (number === 0 || number == NaN || number == null) {
+    number = 1;
+  }
+  return number;
+}
+function calculate() {
+  for (let i = 0; i < count; i++) {
+    let BEno = document.getElementById("BEno" + i);
+    let date = document.getElementById("date" + i);
+    let name = document.getElementById("name" + i);
+    let HScode = document.getElementById("HScode" + i);
+    let quantity = document.getElementById(`quantity${i}`);
+    let purchaseVal = document.getElementById(`purchaseVal${i}`);
+    let purchaseRate = document.getElementById(`purchaseRate${i}`);
+    let additionRate = document.getElementById(`additionRate${i}`);
+    let additionVal = document.getElementById(`additionVal${i}`);
+    let salesRate = document.getElementById(`salesRate${i}`);
+    let salesVal = document.getElementById(`salesVal${i}`);
+    let vatRate = document.getElementById(`vatRate${i}`);
+    let vatVal = document.getElementById(`vatVal${i}`);
+    let rebate = document.getElementById(`rebate${i}`);
+    let at = document.getElementById(`at${i}`);
+    let trDeposite = document.getElementById(`trDeposite${i}`);
+    let openingStock = document.getElementById(`openingStock${i}`);
+    let salesQty = document.getElementById(`salesQty${i}`);
+    let closingStock = document.getElementById(`closingStock${i}`);
+    let tr = document.getElementById(`tr${i}`);
+    let closingBal = document.getElementById(`closingBal${i}`);
+
+    BEno = notZero(BEno.value);
+    date = notZero(date.value);
+    name = notZero(name.value);
+    HScode = notZero(HScode.value);
+
+    quantity = notZero(quantity.value);
+    purchaseVal = notZero(purchaseVal.value);
+    purchaseRate.value = (purchaseVal / notZero(parseFloat(quantity))).toFixed(
+      2
+    );
+    purchaseRate = purchaseRate.value;
+    additionRate = notZero(additionRate.value);
+
+    additionVal.value = (
+      parseFloat(purchaseRate) +
+      parseFloat(purchaseRate * additionRate) / 100
+    ).toFixed(2);
+
+    salesRate.value = parseFloat(purchaseRate + Number(additionVal)).toFixed(2);
+    salesRate = salesRate.value;
+    salesVal.value = (salesRate * quantity).toFixed(2);
+    salesVal = salesVal.value;
+    let dropdown = document.getElementById(`vatRate${i}`);
+    vatRate.value = dropdown.value;
+    vatRate = vatRate.value;
+    vatVal.value = ((salesVal * vatRate) / 100).toFixed(2);
+    rebate.value =
+      vatRate === "15"
+        ? ((purchaseVal * 15) / 100).toFixed(2)
+        : ((purchaseVal * 5) / 100).toFixed(2);
+
+    at.value = ((purchaseVal * 5) / 100).toFixed(2);
+    vatVal = vatVal.value;
+    rebate = rebate.value;
+    at = at.value;
+    trDeposite.value = (vatVal - (parseFloat(rebate) + parseFloat(at))).toFixed(
+      2
+    );
+    openingStock = notZero(openingStock.value);
+    salesQty = notZero(salesQty.value);
+    closingStock.value = (
+      parseFloat(quantity) +
+      parseFloat(openingStock) -
+      salesQty
+    ).toFixed(2);
+    trDeposite = trDeposite.value;
+    tr.value = ((trDeposite / notZero(quantity)) * salesQty).toFixed(2);
+    closingStock = closingStock.value;
+    closingBal.value = (
+      ((parseFloat(rebate) + parseFloat(at)) / notZero(quantity)) *
+      closingStock
+    ).toFixed(2);
+  }
+}
+function calculateTotal() {
+  let totalQty = document.getElementById("totalQty");
+  let totalPurchaseVal = document.getElementById("totalPurchaseVal");
+  let totalAdditionVal = document.getElementById("totalAdditionVal");
+  let totalSalesVal = document.getElementById("totalSalesVal");
+  let totalVatVal = document.getElementById("totalVatVal");
+  let totalRebate = document.getElementById("totalRebate");
+  let totalAt = document.getElementById("totalAt");
+  let totalTrDeposite = document.getElementById("totalTrDeposite");
+  let totalSalesQty = document.getElementById("totalSalesQty");
+  let totalTr = document.getElementById("totalTr");
+
+  totalQty.value = 0.0;
+  for (let i = 0; i < count; i++) {
+    totalQty.value =
+      parseFloat(totalQty.value) +
+      parseFloat(document.getElementById("quantity" + i).value);
+  }
+}
+document.addEventListener("keyup", (e) => {
+  calculate();
+  calculateTotal();
+});
