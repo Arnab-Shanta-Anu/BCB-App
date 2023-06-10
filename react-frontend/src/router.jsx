bc91ab24e399f536f74c9aa21afdb4d177700550
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2023-06-10 10:53:14 +0600

    fixed routes

--- react-frontend/src/router.jsx
+++ react-frontend/src/router.jsx
@@ -4,6 +4,11 @@ import Home from "./views/Home";
 import Form_9_1 from "./views/forms/Form-9-1";
 import Form_6_2 from "./views/forms/Form-6-2";
 import Form_4_3 from "./views/forms/Form-4-3";
+import GuestLayout from "./components/GuestLayout";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import AdminLayout from "./components/AdminLayout";
+import Dashboard from "./views/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +34,38 @@ const router = createBrowserRouter([
       {
         path: "/form-4-3",
         element: <Form_4_3 />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/login" />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/dashboard" />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
       },
     ],
   },
