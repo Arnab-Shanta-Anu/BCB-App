ff0cbe8bad4eff9d7d9fd04b4f7568e3abd3dbe6
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2023-06-10 11:15:32 +0600

    added navbar and footer component

--- react-frontend/src/main.jsx
+++ react-frontend/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./router";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar/>
     <RouterProvider router={router} />
+    <Footer/>
   </React.StrictMode>
 );
